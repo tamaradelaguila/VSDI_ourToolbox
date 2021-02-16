96d602e6ca1b934ae1f753305b898c84b5d3921b
tamaradelaguila <tamarama3@gmail.com>
2021-02-16 18:06:26 +0100

    debug + user_settings pwd

--- rootpath/user_settings.m
+++ rootpath/user_settings.m
@@ -1,6 +1,14 @@
 
-path.rootpath = 'C:\Users\User\Documents\UGent_brugge\VSDI_tamaraToolbox\VSDI_ourToolbox\rootpath';
-path.data = 'C:\Users\User\Documents\UGent_brugge\VSDI_tamaraToolbox\ROSmapa_data';
+% path.rootpath = 'C:\Users\User\Documents\UGent_brugge\VSDI_tamaraToolbox\VSDI_ourToolbox\rootpath';
+path.rootpath = pwd;
+ temp.idcs   = strfind(path.rootpath,'\');
+ temp.newdir = path.rootpath(1:temp.idcs(end-1));
+
+path.data = fullfile(temp.newdir, 'ROSmapa_data');
+path.list =fullfile(temp.newdir, 'BVdml');
+path.list = 'C:\Users\User\Documents\UGent_brugge\VSDI_tamaraToolbox\BVdml';
+
+clear temp
 
 addpath(genpath(fullfile(path.rootpath, 'functions')));
 
