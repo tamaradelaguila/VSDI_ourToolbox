93bf718a63fe6e719781ecf9a23d7019d8cfc7e7
tamaradelaguila <tamarama3@gmail.com>
2021-08-30 22:02:25 +0200

    plot_framesoverlaid2: new version from the function to set clim and colormaps among others

---
+++ functions/plot/plot_framesoverlaid2.m
@@ -0,0 +1,85 @@
+%% AUXILIARY FUNCTION
+function  plot_framesoverlaid2(imAct, imBack, logicalpha, plotnow, axH, act_clim, plot_cbar, thresh, custom_map)
+% INPUT
+% 'imAct' - image to display in colors
+% 'imBack' - backgr image
+% 'logicalpha': logic matrix with transparency information (1 = shown pixel)
+% 'plotnow': whether we want to plot the figure (set to 0 if we are providing a axisHandle)
+% 'axH': axes handle in which to plot it, when it's going to be included into a subplot
+% 'act_clim': color limits for 'imAct'. If no color limits is provided for the activity (color) map, use the max
+% and min from the input movie
+% 'plot_cbar' : whether we want to plot the colorbar (useful in subplots)
+
+
+if ~exist('act_clim')
+    act_clim = [min(nonzeros(imAct(:))) max(nonzeros(imAct(:)))];
+elseif isempty(act_clim)
+    act_clim = [min(nonzeros(imAct(:))) max(nonzeros(imAct(:)))];
+end
+
+% if ~exist('thresh')
+%     thresh = 0;
+% elseif isempty(thresh)
+%         thresh = 0;
+% end
+
+if ~exist('plotnow')
+    plotnow= 1;
+elseif isempty(plotnow)
+    plotnow= 1;
+end
+
+if ~exist('plot_cbar')
+    plot_cbar= 1;
+elseif isempty(plot_cbar)
+    plot_cbar= 1;
+end
+
+
+if ~exist('custom_map')
+    colormode= 0;
+elseif isempty(custom_map)
+    colormode= 0;
+else
+    colormode = 1;
+end
+
+% end of input control ------------------------
+if plotnow
+    fig1 = figure;
+end
+
+thresh_polarmap = min(abs(thresh));
+
+ax1 = axes;
+imagesc(imBack);
+colormap(ax1,'bone');
+ax1.Visible = 'off';
+axis image
+
+ax2 = axes;
+% imagesc(ax2,imAct,'alphadata',imAct>thresh);
+imagesc(ax2,imAct,'alphadata',logicalpha);
+
+if colormode == 1
+    colormap(ax2, custom_map);
+else
+    colormap(ax2,polarmap());
+end
+
+caxis(ax2, act_clim);
+ax2.Visible = 'off';
+axis image
+
+if plot_cbar
+    colorbar;
+end
+linkprop([axH ax1 ax2],'Position');
+
+% if plotnow
+%    close (fig1)
+% end
+
+end
+
+%% Created as an independent function 30/08/21 from: auxiliary function inside plot_framesoverlaid
